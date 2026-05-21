6f7053684e96b8553a6a187d930fa11d5ae5e694
Clari <luzclarita122011@gmail.com>
2026-05-21 14:06:25 -0300

    funciones

---
+++ Mejorplan.py
@@ -0,0 +1,50 @@
+# =========================
+# FUNCIONES
+# =========================
+
+def A(x):
+    return 40*x + 200
+
+def B(x):
+    return 70*x + 50
+
+def C(x):
+    return -2*x**2 + 80*x + 100
+
+
+# =========================
+# INGRESO DE DATOS
+# =========================
+
+x = int(input("Ingrese cantidad de horas (0 a 50): "))
+
+if x < 0 or x > 50:
+    print("Valor fuera del dominio")
+else:
+
+    a = A(x)
+    b = B(x)
+    c = C(x)
+
+    print("\nCostos:")
+    print("A(x) =", a)
+    print("B(x) =", b)
+    print("C(x) =", c)
+
+    # =========================
+    # MEJOR PLAN
+    # =========================
+
+    menor = a
+    plan = "A"
+
+    if b < menor:
+        menor = b
+        plan = "B"
+
+    if c < menor:
+        menor = c
+        plan = "C"
+
+    print("\nMejor plan:", plan)
+    print("Costo mínimo:", menor)
